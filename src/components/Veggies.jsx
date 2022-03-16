315d313642559775eeb699669d698baa2b637a4a
JJOosthuizen <joossie021@gmail.com>
2022-03-16 05:29:26 -0700

    initial commit

---
+++ src/components/Veggies.jsx
@@ -0,0 +1,105 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+import { Link } from "react-router-dom";
+function Veggies() {
+  // Declare a new state variable, which we'll call "veggie"
+  const [veggie, setVeggie] = useState([]);
+
+  //useEffect loads function when component gets mounted
+  useEffect(() => {
+    getVeggie();
+  }, []);
+
+  const getVeggie = async () => {
+    const check = localStorage.getItem("veggie");
+    //check to see if there is a localstorage saved else make a new one
+    if (check) {
+      setVeggie(JSON.parse(check));
+    } else {
+      //fetch data and make new localstorage
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9&tags=vegetarian`
+      );
+      const data = await api.json();
+      localStorage.setItem("veggie", JSON.stringify(data.recipes));
+      setVeggie(data.recipes);
+    }
+  };
+
+  return (
+    <div>
+      <Wrapper>
+        <h3>Vegetarian Recipes</h3>
+        <Splide
+          options={{
+            //customize slide options
+            perPage: 3,
+            gap: "5rem",
+          }}
+        >
+          {veggie.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
+                <Card>
+                  <Link to={"/recipe/" + recipe.id}>
+                    <p>{recipe.title}</p>
+                    <img src={recipe.image} alt={recipe.title} />
+                    <Gradient />
+                  </Link>
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
+    </div>
+  );
+}
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    left: 0;
+  }
+
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
+export default Veggies;
